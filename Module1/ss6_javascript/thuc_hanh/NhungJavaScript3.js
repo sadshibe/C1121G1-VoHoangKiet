b4692d56dc25cb2b891f78427d107f50bce01cf6
sadshibe <87393763+sadshibe@users.noreply.github.com>
2021-12-06 16:11:49 +0700

    Add files via upload

---
+++ Module1/ss6_javascript/thuc_hanh/NhungJavaScript3.js
@@ -0,0 +1,3 @@
+function thongBao() {
+    alert("Xin Chào")
+}
---
+++ Module1/ss7_bien_kdl_toan_tu/thuc_hanh/SuDungToanTu.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeigth;
+inputHeigth = prompt('Nhập chiều cao :');
+inputWidth = prompt('Nhập chiều dài :');
+let width =parseInt(inputWidth);
+let heigth =parseInt(inputHeigth);
+let area = width*heigth;
+document.write('Diện tích hình chữ nhật là : '+ area) ;
